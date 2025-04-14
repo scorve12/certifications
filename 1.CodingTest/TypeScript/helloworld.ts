d974f0da7de207c8e5214941e15ce03511810d16
scorve12 <scorve5322@gmail.com>
2025-04-14 18:21:47 +0900

    feat: add js, ts

---
+++ 1.CodingTest/TypeScript/helloworld.ts
@@ -0,0 +1,11 @@
+/**
+ * 설명성. TypeScript를 전역으로 설치합니다(이미 설치되어 있다면 생략 가능):
+ * 
+ *    npm install -g typescript
+ * 
+ **/
+function helloWorld(): void {
+    console.log("Hello, World!");
+}
+
+helloWorld();
