439d572578fac8d8792a7bd973cca4a99123eae6
scorve12 <scorve5322@gmail.com>
2025-05-08 23:27:58 +0900

    fix: improve deepMerge function to prevent prototype pollution

--- 3.Study_Group/Hard/1.js
+++ 3.Study_Group/Hard/1.js
@@ -1,14 +1,21 @@
 // 문제 1: 프로토타입 오염(Prototype Pollution)
 // 이 코드는 깊은 병합 함수를 구현하려고 하지만 보안 취약점이 있습니다
-function deepMerge(target, source) {
-  for (let key in source) {
+const keypass = (key) => {
+  // 키가 __proto__인 경우 true를 반환합니다
+  return key === '__proto__';
+}
+function deepMerge(target = Object.create(null), source) {
+  
+  for (let key of Object.keys(source)) {
+    if (keypass(key)) continue; // __proto__ 키를 건너뜁니다
     if (source[key] && typeof source[key] === 'object') {
-      if (!target[key]) target[key] = {};
+      if (!target[key]) target[key] = Object.create(null);
       deepMerge(target[key], source[key]);
     } else {
       target[key] = source[key];
     }
   }
+
   return target;
 }
 
@@ -19,7 +26,7 @@ const mergedConfig = deepMerge(userConfig, defaultConfig);
 console.log(mergedConfig.theme.color); // 'blue'
 
 // 이제 악의적인 입력을 시도해봅시다
-const malicious = JSON.parse('{"__proto__": {"malicious": true}}');
+const malicious = JSON.parse('{"__proto__": {"malicious": true}}'); // 악의 적인 입력
 deepMerge({}, malicious);
 
 // 모든 객체가 오염되었는지 확인
@@ -29,6 +36,26 @@ console.log(innocent.malicious); // true - 보안 문제!
 /*
 디버깅 도전 과제:
 1. 프로토타입 오염 취약점을 찾아내고 이해하세요
-2. deepMerge 함수를 보안 취약점 없이 안전하게 수정하세요
+  : "__proto__" 속성을 사용하여 객체의 프로토타입을 변경할 수 있습니다
+  : innocent.malicious = true로 인해 innocent 객체의 프로토타입에 malicious 속성이 추가되었습니다
+  : deepMerge({}, malicious) 호출로 인해 모든 객체의 프로토타입이 오염되었습니다
+  : {}: 빈 객체를 생성할 때 Object.prototype을 상속받습니다
+
+
+2. deepMerge 함수를 보안 취약점 없이 안전하게 수정하세요 : 
+  : Object.create(null): 오브젝트를 생성할 때 프로토타입을 null로 설정하여 기본 객체의 속성을 상속받지 않도록 합니다
+
 3. 악의적인 입력에도 프로토타입이 오염되지 않도록 방어 코드를 추가하세요
+  function deepMerge(target = Object.create(null), source) {
+     for (let key of Object.keys(source)) {
+       if (source[key] && typeof source[key] === 'object') {
+         if (!target[key]) target[key] = Object.create(null);
+         deepMerge(target[key], source[key]);
+       } else {
+         target[key] = source[key];
+       }
+     }
+     return target;
+   }
+   
 */
--- 3.Study_Group/Hard/3.js
+++ 3.Study_Group/Hard/3.js
@@ -4,7 +4,7 @@ function UserService() {
     
     this.loadUser = function(userId) {
       // 사용자 로드 시뮬레이션
-      setTimeout(function() {
+      setTimeout(() => {
         this.currentUser = { id: userId, name: `User ${userId}` };
         console.log("현재 사용자:", this.currentUser);
       }, 1000);
@@ -39,7 +39,55 @@ function UserService() {
   /*
   디버깅 도전 과제:
   1. setTimeout 콜백 내부에서 this가 예상과 다르게 바인딩되는 문제를 찾아내세요
+      - this는 UserService 인스턴스를 가리키지 않음
+      - setTimeout의 콜백 함수에서 this는 전역 객체를 가리킴
+      - 따라서 this.currentUser는 undefined가 됨
+      - 해결 방법: 화살표 함수 사용, bind() 메서드 사용, that 변수 사용 등
+      
   2. this 바인딩 문제를 해결하기 위한 여러 방법(화살표 함수, bind, that 변수 등)을 시도해보세요
+      - 화살표 함수 사용 예시:
+      userService.loadUser = (userId) => {
+          setTimeout(() => {
+              this.currentUser = { id: userId, name: `User ${userId}` };
+              console.log("현재 사용자:", this.currentUser);
+          }, 1000);
+      };
+      - bind() 메서드 사용 예시:
+      userService.loadUser = function(userId) {
+          setTimeout(function() {
+              this.currentUser = { id: userId, name: `User ${userId}` };
+              console.log("현재 사용자:", this.currentUser);
+          }.bind(this), 1000);
+      };
+      - that 변수 사용 예시:
+      const that = this;
+      setTimeout(function() {
+          that.currentUser = { id: userId, name: `User ${userId}` };
+          console.log("현재 사용자:", that.currentUser);
+      }, 1000);
   3. 동일한 코드를 클래스 구문을 사용하여 재작성하고 동일한 문제가 발생하는지 확인해보세요
+      - 클래스 구문을 사용한 예시:
+      class UserService {
+          constructor() {
+              this.currentUser = null;
+          }
+          
+          loadUser(userId) {
+              setTimeout(() => {
+                  this.currentUser = { id: userId, name: `User ${userId}` };
+                  console.log("현재 사용자:", this.currentUser);
+              }, 1000);
+          }
+          
+          getCurrentUser() {
+              return this.currentUser;
+          }
+      }
+      
+      // 테스트 실행
+      testUserService();
   4. 비동기 컨텍스트에서 this 바인딩이 어떻게 동작하는지 설명해보세요
+      - 비동기 함수(setTimeout 등) 내에서 this는 전역 객체를 가리킴
+      - 따라서 비동기 함수 내에서 this.currentUser를 사용하면 undefined가 됨
+      - 이를 해결하기 위해 화살표 함수, bind() 메서드, that 변수 등을 사용하여 this 바인딩을 유지해야 함
   */
